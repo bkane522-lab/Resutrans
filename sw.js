3f5417a2e737d68a1ca50c9fe6cd261e22d8ec42
bkane522-lab <bkane522@gmail.com>
2026-07-01 21:58:40 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "resutrans-v2";
-const SHELL = ["./index.html", "./manifest.json", "./icon.svg"];
+const CACHE = "resutrans-v3";
+const SHELL = ["./index.html", "./manifest.json", "./icon.svg", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
